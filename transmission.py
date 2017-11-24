7ab0f5d9e9ef8cbc645e076d1b1cf3caade27a79
Danilo Barbosa <danilobso@gmail.com>
2017-11-24 20:35:34 -0200

    Transmission of 2 images in a row [ALMOST]

--- transmission.py
+++ transmission.py
@@ -19,135 +19,141 @@ parser.add_argument('--wait', '-w', dest='wait', default=0.2, action="store", ty
 
 class LoRaBeacon(LoRa):
 
-    f = []
-    b = []
-    a = []
-    c =  0
-    i = 0
-    last = 1
-    n=0
-    step = 253
-    sending = 0
-    resto = 0
+	
+	image_buffer = []
+	image_buffer = []
+	buffer_size = []
+	iteration = 0		
+	number_packets = 0
+	step = 253
+	sending = 0
+	resto = 0
+	first_time = 1
   
  
 
-    def __init__(self, verbose=False):
-        super(LoRaBeacon, self).__init__(verbose)
-        self.set_mode(MODE.SLEEP)
-        self.set_dio_mapping([1,0,0,0,0,0])
+	def __init__(self, verbose=False):
+		super(LoRaBeacon, self).__init__(verbose)
+		self.set_mode(MODE.SLEEP)
+		self.set_dio_mapping([1,0,0,0,0,0])
 	
 
-    def re_init(self):
+	def re_init(self):
+		
+		self.image_buffer = []
+		self.buffer_size = []
+		self.iteration = 0		
+		self.number_packets = 0
+		self.step = 253
+		self.sending = 0
+		self.resto  = 0
+		self.first_time = 1
 
-    	self.f = []
-    	self.b = []
-    	self.a = []
-    	self.c =  0
-    	self.i = 0
-    	self.last = 1
-    	self.n=0
-    	self.step = 253
-        self.sending = 0
-        self.resto  = 0
-    
-
-    def take_pic(self):
-    	print "Taking Pic...."
-
-        # raspistill command for taking pictures from Raspberry Cam
-    	os.system("raspistill -th none -vf -w 640 -h 480 -q 10 -o currentPicture.jpg")
-        os.system("convert currentPicture.jpg teste.bmp")
-
-        # Open saved picture
-    	with open("currentPicture.jpg","rb") as imageFile:
-       	    self.f = imageFile.read()
-
-        self.b = bytearray(self.f)
-        self.a = sys.getsizeof(self.b)
-        self.c =  len(self.b)
-        self.n = int( self.a / self.step)
-
-    def send_image(self):     
-
-        if self.sending == 1:
-
-            # Normal packet
-            if self.i < self.n:
-
-                # Appending packet number, 1 byte
-                Z = bytearray([self.i,0])
-
-                # Appending bytearray from picture, 253 bytes
-                Z[1:] = self.b[self.i * self.step : self.i * self.step + self.step]                
-                print "I'm on %d" % self.i
-
-                # Casting to List (needed)
-                E = list(Z)     
-                
-                # Increment Packet number
-                self.i += 1
-
-                # Init SPI transmission to radio
-                self.write_payload(E)
-                BOARD.led_on()
-                self.set_mode(MODE.TX)
-
-            # Last packet
-            elif self.i == self.n: 
-
-                print "Sending remaining bytes....."           
-                # Appending packet number, 1 byte
-                Z = bytearray([self.i,0])
-                print "Packet: %d" % self.i
-
-                # Appending bytearray from picture, X remaining bytes
-                Z[1:] = self.b[self.i * self.step:]            
-
-                # Casting to List (needed)
-                E = list(Z) 
-
-                # Init SPI transmission do radio
-                self.write_payload(E)
-                BOARD.led_on()
-                self.set_mode(MODE.TX)
-
-                # Reset global variables
-                self.re_init()
-
-                print "Waiting for new pic..."
-
-    def on_tx_done(self):
-        global args
-        self.set_mode(MODE.STDBY)
-        self.clear_irq_flags(TxDone=1)
-        BOARD.led_off()
-        sleep(args.wait)
-        if self.sending == 1:
-            self.send_image()    
-
-    # Main function
-    def start(self):
-        global args                
-        BOARD.led_on()
+	def semi_re_init(self):
+		self.iteration = 0
 	
-	while True:
-	    sleep(0.5) # Needed for some unknown reason.
 
-	    if self.sending == 0: # If not sending
-	    	print "Starting process"
-	    	sleep(4)
+	def take_pic(self):
+		print "Taking Pic...."
 
-	    	self.sending = 1
-	    	self.take_pic() # Take new picture
+		# raspistill command for taking pictures from Raspberry Cam
+		os.system("raspistill -th none -vf -w 640 -h 480 -q 10 -o currentPicture.jpg")
+		os.system("convert currentPicture.jpg teste.bmp")
 
-	    	print "TAMANHO DA FOTO %d" % self.a 
-	    	print self.n
-	    	print "PACOTES CHEIOS: %d" % self.n
-	    	print "RESTO: %d" % (self.a%self.step)
-	    	
-	    	self.send_image() # Iniciate IRQ recurssion
-	    	  	
+		# Open saved picture
+		with open("currentPicture.jpg","rb") as imageFile:
+			f = imageFile.read()
+
+		self.image_buffer = bytearray(f)
+		self.buffer_size = sys.getsizeof(self.image_buffer)
+		self.number_packets = int( self.buffer_size / self.step)
+
+	def send_image(self):     
+
+		if self.sending == 1:
+
+			# Normal packet
+			if self.iteration < self.number_packets:
+
+				# Appending packet number, 1 byte
+				Z = bytearray([self.iteration,0])
+
+				# Appending bytearray from picture, 253 bytes
+				Z[1:] = self.image_buffer[self.iteration * self.step : self.iteration * self.step + self.step]                
+				print "I'm on %d" % self.iteration
+
+				# Casting to List (needed)
+				E = list(Z)     
+				
+				# Increment Packet number
+				self.iteration += 1
+
+				# Init SPI transmission to radio
+				self.write_payload(E)
+				BOARD.led_on()
+				self.set_mode(MODE.TX)
+
+			# Last packet
+			elif self.iteration == self.number_packets: 
+
+				print "Sending remaining bytes....."           
+				# Appending packet number, 1 byte
+				Z = bytearray([self.iteration,0])
+				print "Packet: %d" % self.iteration
+
+				# Appending bytearray from picture, X remaining bytes
+				Z[1:] = self.image_buffer[self.iteration * self.step:]            
+
+				# Casting to List (needed)
+				E = list(Z) 
+
+				# Init SPI transmission do radio
+				self.write_payload(E)
+				BOARD.led_on()
+				self.set_mode(MODE.TX)
+
+				# Reset global variables
+				if self.first_time == 0:		
+					self.re_init()
+					print "Waiting for new pic..."
+				else:
+					self.semi_re_init()
+					print "Sending picture second time..."
+					self.first_time = 0
+
+	def on_tx_done(self):
+		global args
+		self.set_mode(MODE.STDBY)
+		self.clear_irq_flags(TxDone=1)
+		BOARD.led_off()
+		sleep(args.wait)
+		if self.sending == 1:
+			self.send_image()    
+
+	# Main function
+	def start(self):
+		global args                
+		BOARD.led_on()
+	
+		while True:
+
+			sleep(0.5) # Needed for some unknown reason.
+
+			if self.sending == 0: # If not sending
+				print "Starting process..."
+				#sleep(4)
+
+				self.sending = 1
+								
+				self.take_pic() # Take new picture
+					
+				print "Sending picture first time..."
+				print "Picture size: %d bytes." % self.buffer_size 
+				print "Full packets: %d" % self.number_packets
+				print "Last packet: %d bytes." % (self.buffer_size%self.step)
+
+				self.send_image() # Iniciate IRQ recurssion            
+				
 
 lora = LoRaBeacon(verbose=False)
 args = parser.parse_args(lora)
@@ -160,12 +166,12 @@ print "Beacon config: %d" % args.wait
 print 
 
 try:
-    lora.start()
+	lora.start()
 except KeyboardInterrupt:
-    sys.stdout.flush()
-    sys.stderr.write("KeyboardInterrupt\n")
+	sys.stdout.flush()
+	sys.stderr.write("KeyboardInterrupt\n")
 finally:
-    sys.stdout.flush()
-    print 
-    lora.set_mode(MODE.SLEEP)
-    BOARD.teardown()
+	sys.stdout.flush()
+	print 
+	lora.set_mode(MODE.SLEEP)
+	BOARD.teardown()
